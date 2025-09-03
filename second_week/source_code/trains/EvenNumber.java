df50ca7646d7804e6f6e19b95a54896e2b82f561
Adhim Niokagi <niokagi@gmail.com>
2025-09-04 02:04:35 +0700

    code EvenNumber and multiple of 6

--- second_week/source_code/trains/EvenNumber.java
+++ second_week/source_code/trains/EvenNumber.java
@@ -2,6 +2,11 @@ package trains;
 
 public class EvenNumber {
     public static void main(String[] args) {
-
+        for (int i = 2; i <= 20; i++) {
+            if (i % 2 == 0 && i % 6 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
     }
 }

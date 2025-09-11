a802c0049a0074370760868274110c93be79613f
Adhim Niokagi <niokagi@gmail.com>
2025-09-12 05:32:59 +0700

    create LocationTester.java

---
+++ third_week/source_codes/tries/LocationTester.java
@@ -0,0 +1,13 @@
+public class LocationTester {
+
+    public static void main(String[] args) {
+        Location surabaya = new Location(112.7521, -7.2575);
+        Location jakarta = new Location(106.8456, -6.2088);
+
+        System.out.println("Lokasi A (Surabaya): Lat=" + surabaya.latitude + ", Lon=" + surabaya.longitude);
+        System.out.println("Lokasi B (Jakarta): Lat=" + jakarta.latitude + ", Lon=" + jakarta.longitude);
+
+        double jarak = surabaya.distanceTo(jakarta);
+        System.out.printf("Hasil perhitungan jarak: %.2f km\n", jarak);
+    }
+}

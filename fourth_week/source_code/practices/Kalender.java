a1b3834f6749e6e317991664353d1340e2d84be7
Adhim Niokagi <niokagi@gmail.com>
2025-09-18 23:59:11 +0700

    Create Kalender class at practice section

---
+++ fourth_week/source_code/practices/Kalender.java
@@ -0,0 +1,5 @@
+package fourth_week.source_code.practices;
+
+public class Kalender {
+
+}
---
+++ fourth_week/source_code/practices/KalenderTester.java
@@ -0,0 +1,7 @@
+package fourth_week.source_code.practices;
+
+public class KalenderTester {
+    public static void main(String[] args) {
+
+    }
+}

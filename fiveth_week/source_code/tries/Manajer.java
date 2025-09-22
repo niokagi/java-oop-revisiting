0251ff0948306a46453653903da9e0d74b006c5c
Adhim Niokagi <niokagi@gmail.com>
2025-09-22 16:36:56 +0700

    update Manajer.java

--- fiveth_week/source_code/tries/Manajer.java
+++ fiveth_week/source_code/tries/Manajer.java
@@ -2,6 +2,7 @@ public class Manajer extends Pegawai {
     public String departemen;
 
     public void IsiData(String n, String d) {
+        // sengaja dibiarkan error untuk kebutuhan analisis
         nama = n;
         departemen = d;
     }

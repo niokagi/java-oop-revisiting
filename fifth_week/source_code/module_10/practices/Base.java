22d502bd8f83c606a0ae1cf88e5ec9510126cb88
Adhim Niokagi <niokagi@gmail.com>
2025-09-24 02:44:30 +0700

    Create Base and Sup classes

---
+++ fifth_week/source_code/module_10/practices/Base.java
@@ -0,0 +1,10 @@
+package fifth_week.source_code.module_10.practices;
+
+public class Base {
+    Base(int i) {
+        System.out.println("base constructor");
+    }
+
+    Base() {
+    }
+}
---
+++ fifth_week/source_code/module_10/practices/Sup.java
@@ -0,0 +1,16 @@
+package fifth_week.source_code.module_10.practices;
+
+public class Sup {
+    public static void main(String argv[]) {
+        Sup s = new Sup();
+        // baris 1
+    }
+
+    Sup() {
+        // baris 2
+    }
+
+    public void derived() {
+        // baris 3
+    }
+}
